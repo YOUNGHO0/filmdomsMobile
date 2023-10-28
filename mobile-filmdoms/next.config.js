3badad5aa29fde038ca2a0628cdfc61d3c5f9038
YOUNGHO0 <31pqopq13@gmail.com>
2023-10-28 18:11:08 +0900

    Fix : 이미지 경로 추가

--- mobile-filmdoms/next.config.js
+++ mobile-filmdoms/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        domains:['api.filmdoms.studio'],
+    }
+}
 
 module.exports = nextConfig
