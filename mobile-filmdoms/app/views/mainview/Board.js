2ca40edc453ec3ed0b4f60c541a532b3949a7519
YOUNGHO0 <31pqopq13@gmail.com>
2023-10-29 17:36:32 +0900

    Fix : fetch 캐싱 미사용으로 변경

--- mobile-filmdoms/app/views/mainview/Board.js
+++ mobile-filmdoms/app/views/mainview/Board.js
@@ -1,9 +1,9 @@
 import styles from './Board.module.css'
 import TabBoard from "@/app/views/mainview/TabContent";
 export default async function Board(props){
-    let recent = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + '/api/v1/main/recent',{method: 'get'})
+    let recent = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + '/api/v1/main/recent',{method: 'get' ,cache: "no-store"})
         .then(value => value.json())
-    let movie = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + '/api/v1/main/movie',{method: 'get'})
+    let movie = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + '/api/v1/main/movie',{method: 'get' ,cache: "no-store"})
         .then(value => value.json())
 
     return (
