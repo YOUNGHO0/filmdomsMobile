7aee9fb74bf0e573ade6f40d73317bb7ea4fd60d
YOUNGHO0 <31pqopq13@gmail.com>
2023-10-30 15:53:08 +0900

    Remove : 로그 삭제

--- mobile-filmdoms/app/board/[boardName]/TopPost.js
+++ mobile-filmdoms/app/board/[boardName]/TopPost.js
@@ -6,7 +6,6 @@ export default async function TopPost(){
 
     const topPostData = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + '/api/v1/article/top-posts',{method: 'get'})
         .then(value => value.json())
-    console.log(topPostData);
 
     return (
             <TopPostList data = {topPostData.result}></TopPostList>
