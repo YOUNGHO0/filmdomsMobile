14bf5b7816c8ae18451d739f4c352fe561aa4145
YOUNGHO0 <31pqopq13@gmail.com>
2023-11-05 19:01:20 +0900

    Feat : 전체 세부 페이지

---
+++ mobile-filmdoms/app/board/[boardName]/[id]/BoardDetailComponent.js
@@ -0,0 +1,49 @@
+'use client'
+import {useRouter} from "next/navigation";
+import styles from './BoardDetailComponent.module.css'
+import Image from "next/image";
+import {useState} from "react";
+import TagDateAndTitle from "@/app/board/[boardName]/[id]/header/TagAndTitle";
+import AuthorAndInfo from "@/app/board/[boardName]/[id]/header/AuthorAndInfo";
+import ContentAndLikes from "@/app/board/[boardName]/[id]/content/ContentAndLikes";
+import CommentComponent from "@/app/board/[boardName]/[id]/comment/CommentComponent";
+
+function toDate(number){
+    const timestamp = number;
+    var myDate = new Date(timestamp);
+    let year = myDate.getFullYear();
+    let month = myDate.getMonth()+1;
+    let date = myDate.getDate();
+    return (year + '.' + month + '.'+ date)
+}
+
+
+export default function BoardDetailComponent(props){
+
+    let router = useRouter()
+    let boardData = props.boardData.result;
+    let commentData = props.commentData.result;
+
+    let [userLike,changUserLike] = useState(boardData.liked)
+
+    function BackButton() {
+        return <div onClick={() => router.back()} style={{display: "flex"}}>
+            <Image src={'/buttonBack.svg'} width={28} height={28}></Image>
+            <div className={styles.detailBoardName} style={{textTransform: "capitalize"}}> {props.boardName}</div>
+        </div>;
+    }
+
+    return(
+        <div style={{marginTop: "32px"}}>
+            <BackButton></BackButton>
+            <div style={{margin:"26px 20px"}}>
+                <TagDateAndTitle boardData = {boardData} toDate = {toDate}></TagDateAndTitle>
+                <AuthorAndInfo boardData = {boardData} commentCount = {commentData.commentCount}></AuthorAndInfo>
+                <ContentAndLikes boardData = {boardData} userLike = {userLike}></ContentAndLikes>
+            </div>
+            <div>
+                <CommentComponent commentData = {commentData}></CommentComponent>
+            </div>
+        </div>
+    )
+}
--- mobile-filmdoms/app/board/[boardName]/[id]/page.js
+++ mobile-filmdoms/app/board/[boardName]/[id]/page.js
@@ -1,3 +1,14 @@
-export default function hello(props){
-    return <div> 게시글 세부 페이지</div>
+import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
+import BoardDetailComponent from "@/app/board/[boardName]/[id]/BoardDetailComponent";
+
+export default async function hello(props){
+
+    let boardData = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + `/api/v1/article/${props.params.boardName}/${props.params.id}`,{method: 'get'})
+        .then(value => value.json())
+    let commentData = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + `/api/v1/article/${props.params.boardName}/${props.params.id}/comment`,{method: 'get'})
+        .then(value => value.json())
+
+    return (<div>
+            <BoardDetailComponent boardName ={props.params.boardName} boardData = {boardData} commentData = {commentData}></BoardDetailComponent>
+            </div>)
 }
