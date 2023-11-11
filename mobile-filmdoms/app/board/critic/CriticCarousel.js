c243ce761c1e5643f5513b27ce0daeb47592a3bd
YOUNGHO0 <31pqopq13@gmail.com>
2023-11-11 21:43:34 +0900

    Feat : This Weeks Critic 의 Carousel 구현

--- mobile-filmdoms/app/board/critic/CriticCarousel.js
+++ mobile-filmdoms/app/board/critic/CriticCarousel.js
@@ -1,35 +1,26 @@
 'use client'
-import React, { useRef, useState } from 'react';
+import React from 'react';
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import {Swiper, SwiperSlide} from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import './Carousel.css'
-
+import './CriticCarousel.css'
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import {Navigation} from 'swiper/modules';
 import Image from "next/image";
 
-export default function Carousel() {
-    const progressContent = useRef(null);
+export default function CriticCarousel() {
 
     return (
         <Swiper
             spaceBetween={0}
             centeredSlides={true}
-            autoplay={{
-                delay: 5000,
-                disableOnInteraction: false,
-            }}
-            pagination={{
-                clickable: true,
-            }}
-            navigation={false}
-            modules={[Autoplay, Pagination]}
-            className="mySwiper"
+            navigation={true}
+            modules={[Navigation]}
+            className="mySwiper2"
         >
             <SwiperSlide>
                 <Image sizes={"100vw"} width={0} height={0} src={"/carousel.png"}/>
