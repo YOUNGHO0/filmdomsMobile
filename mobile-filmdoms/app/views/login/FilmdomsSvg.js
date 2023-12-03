ba83cce40b89cd813d61f52d9d7eafb68d8e8cd0
YOUNGHO0 <31pqopq13@gmail.com>
2023-12-03 13:37:33 +0900

    Feat : login 페이지 추가

---
+++ mobile-filmdoms/app/views/login/FilmdomsSvg.js
@@ -0,0 +1,34 @@
+export default function FilmdomsSvg(){
+    return(
+        <div style={{width:"100%",marginLeft:"auto",marginRight:"auto"}} >
+            <div style={{display:"flex", justifyContent:"center"}} >
+                <svg style={{marginRight:"9px"}} width="78" height="78" viewBox="0 0 78 78" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M77.3072 20.6293V0H42.9484H0V20.6293V34.3821V55.0114V77.3598H42.9484V55.0114H68.7175V34.3821H42.9484V20.6293H77.3072Z" fill="#F7F7F5"/>
+                </svg>
+                <svg style={{marginRight:"9px"}} width="79" height="78" viewBox="0 0 79 78" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M78.2041 20.6293V0H65.3196H13.7815H0.896973V20.6293H13.7815V56.7305H0.896973V77.3598H13.7815H65.3196H78.2041V56.7305H65.3196V20.6293H78.2041Z" fill="#E96437"/>
+                </svg>
+                <svg style={{marginRight:"9px"}} width="79" height="78" viewBox="0 0 79 78" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M43.7424 42.9777V0H0.793945V42.9777V77.3598H43.7424H78.1011V42.9777H43.7424Z" fill="#FFEB74"/>
+                </svg>
+                <svg width="78" height="78" viewBox="0 0 78 78" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M52.8962 0L39.5908 17.2383L25.9332 0H0.692383V77.3598H17.8717H17.992V51.5474L26.5516 77.3598H52.2563L60.8159 51.5474V77.3598H60.8202H77.9995V0H52.8962Z" fill="#E75B4E"/>
+                </svg>
+            </div>
+            <div style={{marginTop:"8.6px",display:"flex",justifyContent:"center"}}>
+                <svg style={{marginRight:"9px"}} width="78" height="78" viewBox="0 0 78 78" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M37.5026 0.384766H34.3587H0V77.7445H23.1921H29.2049H37.5026C59.4535 77.7445 77.3072 60.3945 77.3072 39.0647C77.3072 17.7348 59.4492 0.384766 37.5026 0.384766ZM31.3523 51.9579V26.1714H39.942V51.9579H31.3523Z" fill="#A0B8FF"/>
+                </svg>
+                <svg style={{marginRight:"9px"}} width="79" height="79" viewBox="0 0 79 79" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.896973 0.956055V78.3158H78.2041V0.956055H0.896973ZM44.2749 52.959H35.6852V27.1724H44.2749V52.959Z" fill="#F7F7F5"/>
+                </svg>
+                <svg style={{marginRight:"9px"}} width="79" height="79" viewBox="0 0 79 79" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M52.9977 0.956055L39.6923 18.1944L26.0347 0.956055H0.793945V78.3158H17.9733H18.0936V52.5035L26.6532 78.3158H52.3578L60.9174 52.5035V78.3158H60.9217H78.1011V0.956055H52.9977Z" fill="#67C05F"/>
+                </svg>
+                <svg width="78" height="79" viewBox="0 0 78 79" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M77.9995 18.1471V0.956055H0.692383V52.5292H43.6408V61.1248H0.692383V78.3158H77.9995V26.7426H35.0511V18.1471H77.9995Z" fill="#F7F7F5"/>
+                </svg>
+            </div>
+        </div>
+    )
+}
