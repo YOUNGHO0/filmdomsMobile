9709e832082c04e494284791b23d42b031607814
YOUNGHO0 <31pqopq13@gmail.com>
2023-12-04 14:15:30 +0900

    Fix: 잘못된 리프레시 토큰이 들어올 시 예외처리

--- mobile-filmdoms/pages/api/v1/account/refresh-token.js
+++ mobile-filmdoms/pages/api/v1/account/refresh-token.js
@@ -3,20 +3,25 @@ import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/post
 
 export default async function handler(request,response){
 
+
+
     let requestCookies = request.cookies;
     if(requestCookies.refreshToken != undefined && requestCookies.accessToken == undefined){
 
-        let result = await withCookiesTo("/api/v1/account/refresh-token",request.method,undefined,request)
-        console.log("토큰 재발급")
-        let cookie = result.headers.get("set-cookie")
-        let noDomainCookie = cookie.map((value,key)=>{
-            return value.replace('.filmdoms.studio',"localhost")
-        })
-        process.env.NODE_ENV == 'development'?
-            response.setHeader("set-cookie",[...noDomainCookie]):response.setHeader("set-cookie",[...cookie])
+        await withCookiesTo("/api/v1/account/refresh-token",request.method,undefined,request)
+            .then((result)=>{setAccessTokenToHeader(result,response)})
+            .catch(()=>{})
     }
-
 
     return response.status(200).json("success");
 
+}
+
+function setAccessTokenToHeader(result,response){
+    let cookie = result.headers.get("set-cookie")
+    let noDomainCookie = cookie.map((value,key)=>{
+        return value.replace('.filmdoms.studio',"localhost")
+    })
+    process.env.NODE_ENV == 'development'?
+        response.setHeader("set-cookie",[...noDomainCookie]):response.setHeader("set-cookie",[...cookie])
 }
