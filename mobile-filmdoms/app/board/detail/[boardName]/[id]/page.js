b52183b43c0d94966f596dc0f4b732ba55e4c886
YOUNGHO0 <31pqopq13@gmail.com>
2023-12-12 18:12:20 +0900

    Fix : 페이지 요청시 사용자 정보도 함께 전송하게 변경 * 좋아요 여부를 확인하기 위함

--- mobile-filmdoms/app/board/detail/[boardName]/[id]/page.js
+++ mobile-filmdoms/app/board/detail/[boardName]/[id]/page.js
@@ -1,12 +1,12 @@
 import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
 import BoardDetailComponent from "@/app/board/detail/[boardName]/[id]/BoardDetailComponent";
+import {getRequestWithCookiesTo} from "@/app/util/pageUtil";
 
 export default async function page(props){
 
-    let boardData = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + `/api/v1/article/${props.params.boardName}/${props.params.id}`,{method: 'get'})
-        .then(value => value.json())
-    let commentData = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + `/api/v1/article/${props.params.boardName}/${props.params.id}/comment`,{method: 'get'})
-        .then(value => value.json())
+
+    let boardData = await getRequestWithCookiesTo(`/api/v1/article/${props.params.boardName}/${props.params.id}`,(value)=>value.data,(res)=>{console.log(res)});
+    let commentData = await getRequestWithCookiesTo(`/api/v1/article/${props.params.boardName}/${props.params.id}/comment`,(value)=>value.data,(res)=>{console.log(res)});
 
     return (<div>
             <BoardDetailComponent boardName ={props.params.boardName} boardData = {boardData} commentData = {commentData}></BoardDetailComponent>
